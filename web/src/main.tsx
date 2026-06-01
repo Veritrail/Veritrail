aef405152f5781654ab685f15161ccfe30a33158
Eliazar Chodjayev <96201125+awakzdev@users.noreply.github.com>
2026-06-01 12:25:02 +0300

    Route findings page to redesigned implementation

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import Login from "./pages/Login";
-import Findings from "./pages/Findings";
+import Findings from "./pages/FindingsV2";
 import Accounts from "./pages/Accounts";
 import AuthCallback from "./pages/AuthCallback";
 import Account from "./pages/Account";
@@ -37,10 +37,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={qc}>
       <BrowserRouter>
         <Routes>
-          {/* Public trust center */}
           <Route path="/trust/:slug" element={<TrustCenter />} />
-
-          {/* User auth routes */}
           <Route path="/login" element={<Login />} />
           <Route path="/security" element={<Security />} />
           <Route path="/auth/callback" element={<AuthCallback />} />
@@ -66,8 +63,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             <Route path="/integrations/gitlab" element={<GitLabIntegration />} />
             <Route path="/integrations/gitlab/edit" element={<GitLabIntegrationEdit />} />
           </Route>
-
-          {/* Auditor routes */}
           <Route path="/auditor/verify/:token" element={<AuditorLogin />} />
           <Route path="/auditor/login" element={<AuditorLogin />} />
           <Route element={<AuditorLayout />}>
@@ -80,5 +75,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
